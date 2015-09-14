91c59373fb514a4c1ec8ab5736f47fdea0bca790
Luke Slater <aberowl@jagannath.pdn.cam.ac.uk>
2015-09-14 11:12:43 +0100

    make site crawlable by google

--- src/GenStaticPage.groovy
+++ src/GenStaticPage.groovy
@@ -6,9 +6,16 @@ import com.gargoylesoftware.htmlunit.javascript.background.*
 
 def query = request.getParameter("site")
 query = java.net.URLDecoder.decode(query, "UTF-8")
-
+def str = request.getParameter("query")
+str = str.replaceAll("_escaped_fragment_=","")
+str = java.net.URLEncoder.encode(str, "UTF-8")
+if (query.startsWith("/")) {
+  query = query.substring(1)
+}
+println "http://aber-owl.net/$query#!$str"
 WebClient webClient = new WebClient(BrowserVersion.FIREFOX_38)
-HtmlPage page = webClient.getPage(query)
+HtmlPage page = webClient.getPage("http://aber-owl.net/$query#!$str")
+
 JavaScriptJobManager manager = page.getEnclosingWindow().getJobManager();
 while (manager.getJobCount() > 0) {
   Thread.sleep(1000);
